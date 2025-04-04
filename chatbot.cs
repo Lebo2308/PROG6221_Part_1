a0a7db0e21e4175d9e0b41fc834d2254d5d0ca5d
Lebo2308 <st10349188@rcconnect.edu.za>
2025-04-04 20:53:17 +0000

    added more information on responds

--- chatbot.cs
+++ chatbot.cs
@@ -110,9 +110,18 @@ namespace PROG6221_Part_1
                 replies.Add("I am good thanks and you");
                 replies.Add("My purpose is to simulate real-life scenarios where users might encounter cyber \nthreats and provide guidance on avoiding common traps.");
                 replies.Add("You can ask me questions about password safety, phishing and recognising suspicious links.");
-                replies.Add("Passwords need to be protected and kept safe.");
-                replies.Add("SQL injection is very low based on rate.");
-                replies.Add("Attacking phones is based on phishing.");
+                replies.Add("Passwords need to be protected and kept safe. Password need to be atleast 12 characters\n long, they need to include a mix of uppercase and lowercase letters, numbers, and special characters."
+                    + "\nDo not create or use easily guessable information such as your name, birthdate, or common words\n for your passwords. You can use Two-Factor Authentication to add an extra layer of security. Avoid"
+                    + "\nreusing password across multiple accounts and never share your passwords with other people. \nTips you can use:\nAvoid entering sensitive information or logging into accounts on public Wi-Fi networks." 
+                   + "\n You can use a secure browser, such as Tor, to protect your online activity.\nRegularly update your operating system, browser, and other software to ensure you have the latest security patches.");
+
+                replies.Add("Safe browsing is essential to protect yourself from online threats, such as malware, phishing, and ransomware.\nSafe Browsing Tips:\nKeep your browser and operating system up-to-date: Ensure you have the latest security patches and updates." +
+                    "\nUse antivirus software: Install and regularly update antivirus software to protect against malware.\nAvoid using public computers or public Wi-Fi because they may be compromised by malware or hackers.\nUse a VPN: Consider using a virtual private network (VPN) to encrypt your internet traffic." +
+                    "\nOnly download files from trusted sources, and avoid downloading files with suspicious extensions.\nCheck the URL: Verify the website's URL is legitimate and spelled correctly.\n Ensure the website uses HTTPS (Hypertext Transfer Protocol Secure) instead of HTTP.\nCheck for misspellings and grammatical errors because legitimate websites usually don't have errors");
+
+                replies.Add("Phishing is a type of cybercrime that involves tricking individuals into revealing sensitive information,\n such as passwords, credit card numbers, or personal data. You can spot phishing by: \nSuspicious Sender: Verify the sender's email address or phone number.\n" +
+                    "Urgent or Threatening Language: Be cautious of messages that create a sense of urgency or threat.\nSpelling and Grammar Mistakes: Legitimate messages usually don't contain errors.\n Suspicious Links or Attachments " +
+                    "You can protect yourself from phishing by: Verifying information; using Strong Passwords; enabling Two-Factor authentication;\n keeping Software Up-to-Date and Be Cautious with Public Wi-Fi.");
             }//end of storing answers method
 
             // Method for storing ignored words
