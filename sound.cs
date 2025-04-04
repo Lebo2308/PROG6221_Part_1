2d15060286acfd2cc7a8cad144ea662639104b08
Lebo2308 <st10349188@rcconnect.edu.za>
2025-04-04 16:46:17 +0000

    i changed the path  and combined the paths to play the audio and used soundplayer to play the audio

--- sound.cs
+++ sound.cs
@@ -1,9 +1,41 @@
-﻿namespace PROG6221_Part_1
+﻿using System.IO;
+using System.Media;
+using System;
+
+namespace PROG6221_Part_1
 {
     public class sound
     {
+        //constructor
         public sound()
         {
-        }
+            //getting the app full location
+            string full_location = AppDomain.CurrentDomain.BaseDirectory;
+
+            //replace bin\Debug\
+            string new_path = full_location.Replace("bin\\Debug\\", "");
+
+            //combine the paths
+            string full_path = Path.Combine(new_path, "CYBERREC.wav");
+
+            //try and catch to play the audio
+            try
+            {
+                //making use of using functions
+                using (SoundPlayer play = new SoundPlayer(full_path))
+                {
+                    //play the file
+                    play.PlaySync();
+
+                }//end of using
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine(error.Message);
+
+            }//end of catch
+
+        }//end of constructor
     }
 }
